6849ec5ede8249f64eea4d7888b76d67eff50a38
Patrick Baselier <patrick.baselier@gmail.com>
2010-11-21 10:32:36 +0100

    Initial create.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbnAmroOnRails::Application.config.secret_token = 'c5b7e0e37ae337e13a73cc6d3661fb03a9e15713b4a23c3d1f32bdb5ee288c6edacedcd32293c5f844b23141fdc8f9a98493c4e0bb0cb793abd94204597ed872'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AbnAmroOnRails::Application.config.session_store :cookie_store, :key => '_abn-amro-on-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AbnAmroOnRails::Application.config.session_store :active_record_store
