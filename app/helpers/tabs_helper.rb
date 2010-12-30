90475b79e25ff8c0f812d1c07e80b3110f07e292
Patrick Baselier <patrick.baselier@gmail.com>
2010-12-30 11:12:45 +0100

    Introduced jquery tabs and refactored a lot of views.

---
+++ app/helpers/tabs_helper.rb
@@ -0,0 +1,67 @@
+# This module is borrowed from https://github.com/CodeOfficer/jquery-ui-rails-helpers
+# Changed to Rails 3 with the help of http://dougselph.com/rails/2010/06/rails3_tabsrenderer/
+# And add some modification.
+# TODO: write specs!!!
+module TabsHelper
+
+  def tabs_for(*options, &block)
+    raise ArgumentError, "Missing block" unless block_given?
+
+    tabs = TabsHelper::TabsRenderer.new( *options, &block )
+    tabs.render.html_safe
+  end
+
+  class TabsRenderer
+    def initialize(options={}, &block)
+      raise ArgumentError, "Missing block" unless block_given?
+
+      @template = eval('self', block.binding)
+      @options = options
+      @tabs = []
+      @routes = Rails.application.routes
+
+      yield self
+    end
+
+    def create(tab_id, tab_text, options={}, &block)
+      # raise "Block needed for TabsRenderer#CREATE" unless block_given?
+      @tabs << [tab_id, tab_text, options, block]
+    end
+
+    def render
+      content_tag(:div, (render_tabs.html_safe + render_bodies.html_safe), { :id => :tabs }.merge(@options))
+    end
+
+    private #  ---------------------------------------------------------------------------
+
+    def render_tabs
+      content_tag(:ul, render_headers.html_safe)
+    end
+
+    def render_headers
+      @tabs.collect do |tab|
+        content_tag(:li, link_to(content_tag(:span, tab[1]), url(tab[0])), tab[2].delete(:item_html))
+      end.join.to_s
+    end
+
+    def render_bodies
+      @tabs.collect do |tab|
+        content = tab[3] ? capture(&tab[3]) : nil
+        if content
+          content_tag(:div, content, tab[2].merge(:id => tab[0]))
+        end
+      end.join.to_s
+    end
+
+    def method_missing(*args, &block)
+      @template.send(*args, &block)
+    end
+
+    def url(id_or_url)
+      @routes.recognize_path id_or_url
+      id_or_url
+    rescue ActionController::RoutingError
+      "##{id_or_url}"
+    end
+  end
+end
--- features/manage_uploads.feature
+++ features/manage_uploads.feature
@@ -2,19 +2,19 @@ Feature: Manage uploads
   In order to manage my uploads
   As an authorized user
   I want to view of list of my uploads
-  
+
   Scenario: Listing my uploads
     Given I go to the uploads page
     Then I should see "Listing Uploads"
     And I should see "Uploads" highlighted in the menu
-    
+
   Scenario: New an upload
     Given I go to the uploads page
     When I follow "New Upload" in the submenu
     Then I should see "Add a new Upload"
     And I should see "New Upload" highlighted in the submenu
     And I should see "Uploads" highlighted in the menu
-  
+
   Scenario: Upload no file
     Given I am on the new upload page
     And I press "Save"
@@ -35,12 +35,13 @@ Feature: Manage uploads
     And I should see "There was a problem with your submission. Errors have been highlighted below."
     And I should see "The filename must match the format TXT[yymmddhhmmss].tab. Please try again." for "File"
     But I should not see "The file must be a Tab-delimited text file. Please try again."
-    
+
   Scenario: Upload a valid file
     Given I am on the new upload page
     And I attach the upload file "TXT101204150043.TAB" to "File"
     And I press "Save"
     Then I should be on the upload page for "TXT101204150043.TAB"
+    And show me the page
     And I should see "'TXT101204150043.TAB' was successfully imported."
     And I should the following upload:
       | file_name           | downloaded_at           | upload_details_count | expenses_count |
